12182ef61faa02f00b6fb33b975e464abaf2e342
Reece Hill <29638128+reecehill@users.noreply.github.com>
2022-07-05 23:21:56 +0100

    launcher file that gets missing data and calls wsl

--- Batch_Scripts/freesurfer_batch.sh
+++ Batch_Scripts/freesurfer_batch.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-export FS_LICENSE=/mnt/c/Users/Reece/Documents/Dissertation/freesurfer/license.txt
+export FS_LICENSE=$1
 export FREESURFER_HOME=/usr/local/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
-export pathto_participants=/mnt/c/Users/Reece/Documents/Dissertation/Main/Participants
+export pathto_participants=$2
 
 
 cd $pathto_participants
 i=0
 for subj in $(cat $pathto_participants/file_list_HCP_all_subset.txt);do
 ((i += 1))
-export SUBJECTS_DIR=$pathto_participants/$subj/T1w
 
-# Get T1.nii from T1.mgz, but do NOT place in same directory. 
-mri_convert $SUBJECTS_DIR/$subj/mri/T1.mgz $SUBJECTS_DIR/T1.nii.gz
 
+export SUBJECTS_DIR=$pathto_participants/$subj/unprocessed/3T/T1w_MPR1
 
 ### recon-all to processing data
-recon-all -i $SUBJECTS_DIR/T1.nii.gz -s $subj -all
+mri_convert "${SUBJECTS_DIR}/${subj}_3T_T1w_MPR1.nii.gz" "${SUBJECTS_DIR}/${subj}_3T_T1w_MPR1.nii"
+recon-all -i "${SUBJECTS_DIR}/${subj}_3T_T1w_MPR1.nii" -s "bert" -all
+exit;
+
+
 ### get aparc+aseg.nii
-mri_convert $SUBJECTS_DIR/$subj/mri/aparc+aseg.mgz $SUBJECTS_DIR/$subj/mri/aparc+aseg.nii
+mri_convert $SUBJECTS_DIR/bert/mri/aparc+aseg.mgz $SUBJECTS_DIR/bert/mri/aparc+aseg.nii
 ### get 68 ROI labels based on pial file
-mri_annotation2label --subject $subj --hemi lh --surf pial --outdir $SUBJECTS_DIR/$subj/label/label_type2
-mri_annotation2label --subject $subj --hemi rh --surf pial --outdir $SUBJECTS_DIR/$subj/label/label_type2
+mri_annotation2label --subject "bert" --hemi lh --surf pial --outdir $SUBJECTS_DIR/bert/label/label_type2
+mri_annotation2label --subject "bert" --hemi rh --surf pial --outdir $SUBJECTS_DIR/bert/label/label_type2
 ### get 68 ROI labels based on pial.surf.gii file
-#mri_annotation2label --subject $subj --hemi lh --surf pial.surf.gii --outdir $SUBJECTS_DIR/$subj/label/label_type1
-#mri_annotation2label --subject $subj --hemi rh --surf pial.surf.gii --outdir $SUBJECTS_DIR/$subj/label/label_type1
+#mri_annotation2label --subject "bert" --hemi lh --surf pial.surf.gii --outdir $SUBJECTS_DIR/bert/label/label_type1
+#mri_annotation2label --subject "bert" --hemi rh --surf pial.surf.gii --outdir $SUBJECTS_DIR/bert/label/label_type1
 
 done
--- Batch_Scripts/launch.ps1
+++ Batch_Scripts/launch.ps1
@@ -1,2 +1,45 @@
+# Paths must begin from after the drive number. i.e., (C:\Users\... becomes \Users\...)
+$pathToFreeSurferLicence = "Users/Reece/Documents/Dissertation/freesurfer/license.txt"
+$pathToParticipants = "Users/Reece/Documents/Dissertation/Main/Participants"
+
+# The drive letter that contains all files. In format like - C:\
+$drive = 'C:/'
+
+$startAfresh = Read-host 'Delete all participant information and start afresh? (Yes/No)'
+
+$subjectList = Get-Content -Path $($drive + $pathToParticipants+'\file_list_HCP_all_subset.txt')
+
+if("Yes" -eq $startAfresh) {
+  Rename-Item $($drive + $pathToParticipants) $($drive + $pathToParticipants + '-old')
+  New-Item -Path $($drive + $pathToParticipants) -ItemType Directory
+  Copy-Item $($drive + $pathToParticipants + '-old/file_list_HCP_all_subset.txt') $($drive + $pathToParticipants + '/file_list_HCP_all_subset.txt')
+  Copy-Item $($drive + $pathToParticipants + '-old/.gitignore') $($drive + $pathToParticipants + '/.gitignore')
+  Remove-Item $($drive + $pathToParticipants + '-old') -Recurse
+  Write-Host "The participant folder has been cleared. Please rerun the command and answer 'No'".
+  exit;
+}
+
+# Loop through subjectIds
+foreach ($subjectId in $subjectList){
+  # Check that the raw data for this subject exists.
+  $rawFileDirectory = $($drive + $pathToParticipants + '\' + $subjectId + '\unprocessed\3T\T1w_MPR1\')
+  $rawFileName = $($subjectId) + '_3T_T1w_MPR1.nii.gz'
+  $rawFilePath = $($rawFileDirectory+$rawFileName)
+  if (-not(Test-Path -Path $rawFilePath -PathType Leaf)) {
+    Write-Host "The file [$rawFilePath] does not exist."
+     try {
+         $null = New-Item $rawFileDirectory -itemType Directory -Force -ErrorAction Stop
+         Write-Host "The directory [$rawFileDirectory] has been created."
+         $hostPath = "s3://hcp-openaccess/HCP_1200/$subjectId/unprocessed/3T/T1w_MPR1/"+$subjectId+"_3T_T1w_MPR1.nii.gz"
+         & "C:\Program Files\Amazon\AWSCLIV2\aws.exe" "s3" "cp" "$hostPath" "$rawFileDirectory"
+         Write-Host "The file [$($rawFileDirectory+$rawFileName)] has now been created."
+
+        }
+     catch {
+         throw $_.Exception.Message
+     }
+ }
+}
+
 # Launch WSL (Ubuntu 18 environment first)
-wsl -d "Ubuntu-18.04" -u reece "/mnt/c/Users/Reece/Documents/Dissertation/Main/Batch_Scripts/freesurfer_batch.sh";
+wsl -d "Ubuntu-18.04" -u reece /mnt/c/Users/Reece/Documents/Dissertation/Main/Batch_Scripts/freesurfer_batch.sh $("/mnt/c/"+$pathToFreeSurferLicence) $("/mnt/c/"+$pathToParticipants);

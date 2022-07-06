5f86637c3edf9f64999584afdb46584dc2526c4a
Reece Hill <29638128+reecehill@users.noreply.github.com>
2022-07-06 14:56:47 +0100

    get missing (un/pre)processed data

--- Batch_Scripts/freesurfer_batch.sh
+++ Batch_Scripts/freesurfer_batch.sh
@@ -5,28 +5,45 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export pathto_participants=$2
 export dataToUse=$3
 
+function runFreesurferReconAll {
+  ### recon-all to processing data
+  mri_convert "${SUBJECTS_DIR}/${subj}_T1.nii.gz" "${SUBJECTS_DIR}/${subj}_T1.nii"
+  recon-all -i "${SUBJECTS_DIR}/${subj}_T1.nii" -s ${subj} -all
+}
+### get aparc+aseg.nii
+function getAparcAsecNii {
+  mri_convert $SUBJECTS_DIR/bert/mri/aparc+aseg.mgz $SUBJECTS_DIR/bert/mri/aparc+aseg.nii;
+}
+
+function getROILabels {
+  ### get 68 ROI labels based on pial file
+# The pial file is provided by FreeSurfer.
+mri_annotation2label --subject "bert" --hemi lh --surf pial --outdir $SUBJECTS_DIR/bert/label/label_type2
+mri_annotation2label --subject "bert" --hemi rh --surf pial --outdir $SUBJECTS_DIR/bert/label/label_type2
+
+### get 68 ROI labels based on pial.surf.gii file
+# The HCP dataset provides both pial.surf.gii AND alongside pial.  
+#mri_annotation2label --subject "bert" --hemi lh --surf pial.surf.gii --outdir $SUBJECTS_DIR/bert/label/label_type1
+#mri_annotation2label --subject "bert" --hemi rh --surf pial.surf.gii --outdir $SUBJECTS_DIR/bert/label/label_type1
+}
+
+
 cd $pathto_participants
 i=0
 for subj in $(cat $pathto_participants/file_list_HCP_all_subset.txt);do
 ((i += 1))
 
+  export SUBJECTS_DIR=$pathto_participants/$subj/T1
 
-export SUBJECTS_DIR=$pathto_participants/$subj/unprocessed/3T/T1w_MPR1
-
-if [ "$dataToUse" -eq 'u' ]; then 
-### recon-all to processing data
-mri_convert "${SUBJECTS_DIR}/${subj}_3T_T1w_MPR1.nii.gz" "${SUBJECTS_DIR}/${subj}_3T_T1w_MPR1.nii"
-recon-all -i "${SUBJECTS_DIR}/${subj}_3T_T1w_MPR1.nii" -s "bert" -all
-fi;
-
-
-### get aparc+aseg.nii
-mri_convert $SUBJECTS_DIR/bert/mri/aparc+aseg.mgz $SUBJECTS_DIR/bert/mri/aparc+aseg.nii
-### get 68 ROI labels based on pial file
-mri_annotation2label --subject "bert" --hemi lh --surf pial --outdir $SUBJECTS_DIR/bert/label/label_type2
-mri_annotation2label --subject "bert" --hemi rh --surf pial --outdir $SUBJECTS_DIR/bert/label/label_type2
-### get 68 ROI labels based on pial.surf.gii file
-#mri_annotation2label --subject "bert" --hemi lh --surf pial.surf.gii --outdir $SUBJECTS_DIR/bert/label/label_type1
-#mri_annotation2label --subject "bert" --hemi rh --surf pial.surf.gii --outdir $SUBJECTS_DIR/bert/label/label_type1
+  if [ "$dataToUse" = 'u' ]; then 
+    runFreesurferReconAll;
+    getAparcAsecNii;
+    getROILabels;
+  elif [ "$dataToUse" = 'p' ]; then 
+    getAparcAsecNii;
+    getROILabels;
+  else
+    echo "You must enter either u or p."
+  fi;
 
 done
--- Batch_Scripts/launch.ps1
+++ Batch_Scripts/launch.ps1
@@ -4,14 +4,12 @@ $pathToParticipants = "Users/Reece/Documents/Dissertation/Main/Participants"
 
 # The drive letter that contains all files. In format like - C:\
 $drive = 'C:/'
-
-$startAfresh = Read-host 'Delete all participant information and start afresh? (Yes/No)'
+$startAfresh = Read-host 'Delete all participant information and start afresh? (Y/N)'
 $dataToUse = Read-host 'Begin with Freesurfer and unprocessed data [u], or retrieve preprocessed data and skip Freesurfer [p]?'
-
 $subjectList = Get-Content -Path $($drive + $pathToParticipants+'\file_list_HCP_all_subset.txt')
 
-if("Yes" -eq $startAfresh) {
-  Rename-Item $($drive + $pathToParticipants) $($drive + $pathToParticipants + '-old')
+if("Y" -eq $startAfresh) {
+  Rename-Item $($drive + $pathToParticipants) $($drive + $pathToParticipants + '-old') -Force
   New-Item -Path $($drive + $pathToParticipants) -ItemType Directory
   Copy-Item $($drive + $pathToParticipants + '-old/file_list_HCP_all_subset.txt') $($drive + $pathToParticipants + '/file_list_HCP_all_subset.txt')
   Copy-Item $($drive + $pathToParticipants + '-old/.gitignore') $($drive + $pathToParticipants + '/.gitignore')
@@ -19,28 +17,53 @@ if("Yes" -eq $startAfresh) {
   Write-Host "The participant folder has been cleared. Please rerun the command and answer 'No'".
   exit;
 }
+elseif("N" -eq $startAfresh) {
+    # Loop through subjectIds
+  foreach ($subjectId in $subjectList){
 
-# Loop through subjectIds
-foreach ($subjectId in $subjectList){
-  # Check that the raw data for this subject exists.
-  $rawFileDirectory = $($drive + $pathToParticipants + '\' + $subjectId + '\unprocessed\3T\T1w_MPR1\')
-  $rawFileName = $($subjectId) + '_3T_T1w_MPR1.nii.gz'
-  $rawFilePath = $($rawFileDirectory+$rawFileName)
-  if (-not(Test-Path -Path $rawFilePath -PathType Leaf)) {
-    Write-Host "The file [$rawFilePath] does not exist."
-     try {
-         $null = New-Item $rawFileDirectory -itemType Directory -Force -ErrorAction Stop
-         Write-Host "The directory [$rawFileDirectory] has been created."
-         $hostPath = "s3://hcp-openaccess/HCP_1200/$subjectId/unprocessed/3T/T1w_MPR1/"+$subjectId+"_3T_T1w_MPR1.nii.gz"
-         & "C:\Program Files\Amazon\AWSCLIV2\aws.exe" "s3" "cp" "$hostPath" "$rawFileDirectory"
-         Write-Host "The file [$($rawFileDirectory+$rawFileName)] has now been created."
+    if("u" -eq $dataToUse) {
+      # Set the local directory as the subject's root. We only check it contains the _T1.nii.gz file.
+      $rawFileDirectory = $($drive + $pathToParticipants + '\' + $subjectId + '\T1\')
+      $rawFileName = $($subjectId) + '_T1.nii.gz'
+      $rawFilePath = $($rawFileDirectory+$rawFileName)
+      # Set the host's filepath as that of _T1.nii.gz file. Note that we change the folder hierarchy.
+      $hostPath = "s3://hcp-openaccess/HCP_1200/$subjectId/unprocessed/3T/T1w_MPR1/"+$subjectId+"_3T_T1w_MPR1.nii.gz"
+      $recursive = ''
+    }
+    elseif ("p" -eq $dataToUse) {
+      # Set the directory in question as the subject's bert folder (the output of Freesurfer). We only check for aparc+aseg.mgz
+      $rawFileDirectory = $($drive + $pathToParticipants + '\' + $subjectId + '\T1\bert\')
+      $rawFileName = 'mri/aparc+aseg.mgz'
+      $rawFilePath = $($rawFileDirectory+$rawFileName)
+      # Set the host's filepath as that of the subject's bert folder. Note that we change the folder hierarchy.
+      $hostPath = "s3://hcp-openaccess/HCP_1200/$subjectId/T1w/$subjectId/"
+      $recursive = '--recursive'
+    }
+    else {
+      Write-Host "Please ensure you enter either u or p.".
+      exit;
+    }
 
-        }
-     catch {
-         throw $_.Exception.Message
-     }
- }
+    # Check that the necessary data for this subject exists. If it doesn't, get it from AWS.
+    if (-not(Test-Path -Path $rawFilePath -PathType Leaf)) {
+      Write-Host "The file [$rawFilePath] does not exist. Creating it now..."
+      try {
+          $null = New-Item $rawFileDirectory -itemType Directory -Force -ErrorAction Stop;
+          & "C:\Program Files\Amazon\AWSCLIV2\aws.exe" "s3" "cp" "$hostPath" "$rawFileDirectory" $recursive;
+          Write-Host "The file [$rawFileDirectory] has been created.";
+          }
+      catch {
+          throw $_.Exception.Message
+      }
+  }
+  else {
+    Write-Host "The file [$rawFilePath] was found. No need to get from AWS."
+  }
+  }
+
+  # Launch WSL (Ubuntu 18 environment first)
+  wsl -d "Ubuntu-18.04" -u reece /mnt/c/Users/Reece/Documents/Dissertation/Main/Batch_Scripts/freesurfer_batch.sh $("/mnt/c/"+$pathToFreeSurferLicence) $("/mnt/c/"+$pathToParticipants) "$dataToUse";
 }
-
-# Launch WSL (Ubuntu 18 environment first)
-wsl -d "Ubuntu-18.04" -u reece /mnt/c/Users/Reece/Documents/Dissertation/Main/Batch_Scripts/freesurfer_batch.sh $("/mnt/c/"+$pathToFreeSurferLicence) $("/mnt/c/"+$pathToParticipants) $dataToUse;
+else {
+  Write-Host "Ensure you enter either Y or N."
+}

238cd5e354a560587c7fd0dbedaa828b2bbbae69
Reece Hill <29638128+reecehill@users.noreply.github.com>
2022-07-07 12:03:42 +0100

    convert DsiBatch.sh to .ps1

---
+++ Batch_Scripts/dsiBatch.ps1
@@ -0,0 +1,22 @@
+Set-Location $pathToDsiStudio
+Write-host "Commencing dsiBatch.ps1..."
+Write-host "driveAndPathToParticipants: $driveAndPathToParticipants"
+Write-host "subjectId: $subjectId"
+
+if (!(Test-Path $driveAndPathToParticipants/$subjectId/T1w/Diffusion/logs -PathType Container)) {
+    New-Item -ItemType Directory -Force -Path $driveAndPathToParticipants/$subjectId/T1w/Diffusion/logs
+}
+
+Write-host "Generating .src file for diffusion data of subject: $subjectId..."
+Start-Process $pathToDsiStudio/dsi_studio.exe -ArgumentList "--action=src --source=$driveAndPathToParticipants/$subjectId/T1w/Diffusion/data.nii.gz --bval=$driveAndPathToParticipants/$subjectId/T1w/Diffusion/bvals --bvec=$driveAndPathToParticipants/$subjectId/T1w/Diffusion/bvecs --output=$driveAndPathToParticipants/$subjectId/T1w/Diffusion/data.src.gz" -wait -UseNewEnvironment -RedirectStandardOutput "$driveAndPathToParticipants/$subjectId/T1w/Diffusion/logs/dsi-src-output.log" -RedirectStandardError "$driveAndPathToParticipants/$subjectId/T1w/Diffusion/logs/dsi-src-error.log"
+
+Write-host "Reconstructing image (.fib.gz) for diffusion data of subject: $subjectId..."
+# Removed --csf_calibration=1 as it is no longer an option in DSI Studio (It may be included by default: https://groups.google.com/g/dsi-studio/c/dzJYDTdlDQo/m/NJGvdKXSBwAJ)
+# Removed --output_rdi=0 --output_dif=0"  as no longer an option in DSI Studio (Have been moved to other_output: https://dsi-studio.labsolver.org/doc/cli_t2.html)
+Start-Process $pathToDsiStudio/dsi_studio.exe -ArgumentList "--action=rec  --thread_count=4 --source=$driveAndPathToParticipants/$subjectId/T1w/Diffusion/data.src.gz --output=$driveAndPathToParticipants/$subjectId/T1w/Diffusion/data.src.gz.gqi.1.25.fib.gz --method=4 --param0=1.25 --scheme_balance=1 --check_btable=1" -wait -UseNewEnvironment -RedirectStandardOutput "$driveAndPathToParticipants/$subjectId/T1w/Diffusion/logs/dsi-rec-output.log" -RedirectStandardError "$driveAndPathToParticipants/$subjectId/T1w/Diffusion/logs/dsi-rec-error.log"
+
+
+Write-host "Tracking fibres (1/10) for diffusion data of subject: $subjectId..."
+Start-Process $pathToDsiStudio/dsi_studio.exe -ArgumentList "--action=trk --source=$driveAndPathToParticipants/$subjectId/T1w/Diffusion/data.src.gz.gqi.1.25.fib.gz --fiber_count=10000000 --initial_dir=0 --thread_count=4 --fa_threshold=0.02908 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$driveAndPathToParticipants/$subjectId/T1w/bert/mri/aparc+aseg.nii --output=$driveAndPathToParticipants/$subjectId/T1w/Diffusion/1m0.trk" -wait -UseNewEnvironment -RedirectStandardOutput "$driveAndPathToParticipants/$subjectId/T1w/Diffusion/logs/dsi-trk-0-output.log" -RedirectStandardError "$driveAndPathToParticipants/$subjectId/T1w/Diffusion/logs/dsi-trk-0-error.log"
+
+exit
--- Batch_Scripts/dsiBatch.sh
+++ Batch_Scripts/dsiBatch.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
-export pathto_data=/home/campus.ncl.ac.uk/b7071887/Xchen/Demo_high_resolution
-export pathto_dsistudio=/home/campus.ncl.ac.uk/b7071887/Xchen/dsistudio
-cd $pathto_dsistudio/dsi_studio_64
+export pathToData=/home/campus.ncl.ac.uk/b7071887/Xchen/Demo_high_resolution
+export pathToDsiStudio=C:\Users\Reece\Documents\Dissertation\dsi_studio_win
+cd $pathToDsiStudio/dsi_studio_64
 
 i=0
-for subj in $(cat $pathto_data/batch_files/file_list_HCP_all_subset.txt);do
+for subj in $(cat $pathToData/batch_files/file_list_HCP_all_subset.txt);do
     ((i += 1))
 #### generate SRC files from nifti images (.src)
-$pathto_dsistudio/build/dsi_studio --action=src --source=$pathto_data/$subj/data.nii.gz --bval=$pathto_data/$subj/bvals --bvec=$pathto_data/$subj/bvecs --output=$pathto_data/$subj/data.src.gz
+$pathToDsiStudio/build/dsi_studio --action=src --source=$pathToData/$subj/data.nii.gz --bval=$pathToData/$subj/bvals --bvec=$pathToData/$subj/bvecs --output=$pathToData/$subj/data.src.gz
 
 #### image reconstruction (.fib.gz)
-$pathto_dsistudio/build/dsi_studio --action=rec --thread=4 --source=$pathto_data/$subj/data.src.gz --method=4 --param0=1.25 --scheme_balance=1 --check_btable=1 --csf_calibration=1 --output_rdi=0 --output_dif=0
+$pathToDsiStudio/build/dsi_studio --action=rec --thread=4 --source=$pathToData/$subj/data.src.gz --method=4 --param0=1.25 --scheme_balance=1 --check_btable=1 --csf_calibration=1 --output_rdi=0 --output_dif=0
 
 #### fiber tracking (.trk.gz)
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m0.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m0.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m1.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m1.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m2.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m2.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m3.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m3.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m4.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m4.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m5.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m5.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m6.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m6.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m7.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m7.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m8.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m8.trk.gz
 
-$pathto_dsistudio/build/dsi_studio --action=trk --source=$pathto_data/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathto_data/$subj/mri/aparc+aseg.nii --output=$pathto_data/$subj/1m9.trk.gz
+$pathToDsiStudio/build/dsi_studio --action=trk --source=$pathToData/$subj/data.src.gz.odf8.f5.bal.csfc.012fy.gqi.1.25.fib.gz --fiber_count=1000000 --initial_dir=0 --thread_count=4 --seed_plan=0 --interpolation=0 --random_seed=1 --step_size=0.625 --turning_angle=60 --smoothing=0 --min_length=10 --max_length=300 --method=0 --ref=$pathToData/$subj/mri/aparc+aseg.nii --output=$pathToData/$subj/1m9.trk.gz
 
 #### unzip .trk.gz to .trk which can be open in matlab
-gunzip $pathto_data/$subj/1m0.trk.gz
-gunzip $pathto_data/$subj/1m1.trk.gz
-gunzip $pathto_data/$subj/1m2.trk.gz
-gunzip $pathto_data/$subj/1m3.trk.gz
-gunzip $pathto_data/$subj/1m4.trk.gz
-gunzip $pathto_data/$subj/1m5.trk.gz
-gunzip $pathto_data/$subj/1m6.trk.gz
-gunzip $pathto_data/$subj/1m7.trk.gz
-gunzip $pathto_data/$subj/1m8.trk.gz
-gunzip $pathto_data/$subj/1m9.trk.gz
+gunzip $pathToData/$subj/1m0.trk.gz
+gunzip $pathToData/$subj/1m1.trk.gz
+gunzip $pathToData/$subj/1m2.trk.gz
+gunzip $pathToData/$subj/1m3.trk.gz
+gunzip $pathToData/$subj/1m4.trk.gz
+gunzip $pathToData/$subj/1m5.trk.gz
+gunzip $pathToData/$subj/1m6.trk.gz
+gunzip $pathToData/$subj/1m7.trk.gz
+gunzip $pathToData/$subj/1m8.trk.gz
+gunzip $pathToData/$subj/1m9.trk.gz
 
 #### delete orignal .trk.gz files
-rm -f $pathto_data/$subj/1m0.trk.gz
-rm -f $pathto_data/$subj/1m1.trk.gz
-rm -f $pathto_data/$subj/1m2.trk.gz
-rm -f $pathto_data/$subj/1m3.trk.gz
-rm -f $pathto_data/$subj/1m4.trk.gz
-rm -f $pathto_data/$subj/1m5.trk.gz
-rm -f $pathto_data/$subj/1m6.trk.gz
-rm -f $pathto_data/$subj/1m7.trk.gz
-rm -f $pathto_data/$subj/1m8.trk.gz
-rm -f $pathto_data/$subj/1m9.trk.gz
+rm -f $pathToData/$subj/1m0.trk.gz
+rm -f $pathToData/$subj/1m1.trk.gz
+rm -f $pathToData/$subj/1m2.trk.gz
+rm -f $pathToData/$subj/1m3.trk.gz
+rm -f $pathToData/$subj/1m4.trk.gz
+rm -f $pathToData/$subj/1m5.trk.gz
+rm -f $pathToData/$subj/1m6.trk.gz
+rm -f $pathToData/$subj/1m7.trk.gz
+rm -f $pathToData/$subj/1m8.trk.gz
+rm -f $pathToData/$subj/1m9.trk.gz
 done

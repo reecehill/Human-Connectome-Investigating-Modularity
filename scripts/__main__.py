328ceeed046e9eb2eabd1180baddcc843b89c8b7
Reece Hill <29638128+reecehill@users.noreply.github.com>
2023-04-16 18:35:56 +0100

    updated comments

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -5,40 +5,70 @@
 # 3) Directing the pipeline.
 # 4) Handling shell output.
 
+
+# ------------------------------------------------------------
+# [START] main()
+# ------------------------------------------------------------
 def main() -> None:
-  try:  
-    from modules.logger.logger import LoggerClass
-    
-    # ----------
-    # [START] Initializing.
-    # Initialize each step in the pipeline. This includes:
-    # 1) Clearing writeable folders.
-    # 2) Ensuring new filepaths and files are made where necessary.
-    # 3) Check that included modules are available on the system.
-    # ----------
-    logger = LoggerClass()
+    try:
+        from modules.logger.logger import LoggerClass
 
+        # ------------------------------------------------------------
+        # [START] Check environment.
+        # ------------------------------------------------------------
+        try:
+            pass
+        except Exception as e:
+            print(e)
+            exit()
+        # ------------------------------------------------------------
+            # [END] Check environment.
+        # ------------------------------------------------------------
 
-    # --------
-    # [END] Initializing.
-    # --------
+        # ------------------------------------------------------------
+        # [START] Initializing.
+        # Initialize each step in the pipeline. This includes:
+        # 1) Clearing writeable folders.
+        # 2) Ensuring new filepaths and files are made where necessary.
+        # 3) Check that included modules are available on the system.
+        # ------------------------------------------------------------
+        try:
+            logger = LoggerClass()
+        except Exception as e:
+            print(e)
+            exit()
 
-    # ----------
-    # [START] Running pipeline.
-    # Run each step in the pipeline. See each class "run" function for what occurs.
-    # ----------
+        # ------------------------------------------------------------
+        # [END] Initializing.
+        # ------------------------------------------------------------
 
-    # Get logger instance
-    logger = logger.run()
-    logger.info("Logger ran successfully.")
+        # ------------------------------------------------------------
+        # [START] Running pipeline.
+        # Run each step in the pipeline. See each class "run" function for what occurs.
+        # ------------------------------------------------------------
 
+        try:
+            # ********
+            # LOAD THE LOGGER
+            # ********
+            logger = logger.run()
+            logger.info("Logger ran successfully.")
 
+            # After this point, the logger is loaded so we can use this for improved logging.
+            # From here, logs are shown both in-console and file.
 
+        except Exception as e:
+            print(e)
+            exit()
+        # ------------------------------------------------------------
+        # [END] Running pipeline.
+        # ------------------------------------------------------------
+    except Exception as e:
+        print(e)
+        exit()
+# ------------------------------------------------------------
+# [END] main()
+# ------------------------------------------------------------
 
-
-    # --------
-    # [END] Running pipeline.
-    # --------
-  except Exception as e:
-    print(e)
-    exit()
+if __name__ == "__main__":
+    main()
